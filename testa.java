af547c1227fef6c8923d014576887c51fde909a7
jou.sepe <plamonfort@gmail.com>
2023-12-04 20:09:35 +0100

    primer commit del menu

---
+++ testa.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class testa {
+
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" Pulsa una opción:");
+        System.out.println(" 1. sumar");
+        System.out.println(" 2. restar");
+        System.out.println(" 3. multiplicar:");
+        System.out.println(" 4. dividir");
+        System.out.println(" 5. salir");
+
+        int respuesta = sc.nextInt();
+        
+    }
+
+}
